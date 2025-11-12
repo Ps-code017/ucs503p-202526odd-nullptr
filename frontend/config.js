3fac26c04fdafe7694654eeb3cde31746aa71c0e
Pranjal Satti <pranjalsatti@gmail.com>
2025-11-12 11:20:58 +0530

    deployed backend added

--- frontend/config.js
+++ frontend/config.js
@@ -3,5 +3,5 @@ const CONFIG = {
   API_BASE_URL:
     window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1"    
       ? "http://localhost:3000" // ✅ your local backend
-      : "https://your-backend.onrender.com", // ✅ your deployed backend
+      : "https://ucs503p-202526odd-nullptr.onrender.com", // ✅ your deployed backend
 };
